2be32f1d991d233e12fc1e5a3181705b8239389a
пк <@DESKTOP-81SKH73>
2024-10-17 22:31:25 +0200

    Добавьте файлы проекта.

---
+++ lab2.1/Program.cs
@@ -0,0 +1,19 @@
+﻿try
+{
+    Console.WriteLine("Введите x");
+    double x = double.Parse(Console.ReadLine());
+    Console.WriteLine("Введите y");
+    double y = double.Parse(Console.ReadLine());
+    if (y <= 1 && y >= Math.Abs(x))
+    {
+        Console.WriteLine("Точка (x, y) находится внутри области.");
+    }
+    else
+    {
+        Console.WriteLine("Точка (x, y) находится вне области.");
+    }
+}
+catch
+{
+    Console.WriteLine("Введите корректные данные");
+}
